ff8e1829bc601e11f3261137a0286181b6da96b6
Kleberb82 <kleberbezerra@gmail.com>
2025-09-03 21:38:34 -0300

    Atulização da Class Livro.java

--- src/pessoal/modelo/Livro.java
+++ src/pessoal/modelo/Livro.java
@@ -1,5 +1,41 @@
 package pessoal.modelo;
 
-public class Livro {
+public class Livro extends Item {
+	private String isbn;
+	private int anoPublicacao;
+	
+	public Livro (String titulo, String AutorArtista, Categoria categoria, String isbn, int anoPublicacao) {
+		super(titulo, AutorArtista,categoria);
+		this.isbn = isbn;
+		this.anoPublicacao = anoPublicacao;
+		}
+	
+	public String getIsbn() {
+		return isbn;
+	}
+	
+	public void setIsbn (String isbn) {
+		this.isbn = isbn;
+	}
+	
+	public int getAnoPublicacao() {
+		return anoPublicacao;
+	}
+	
+	public void setAnoPublicacao(int anoPublicacao) {
+		this.anoPublicacao = anoPublicacao;
+	}
+	
+	@Override
+	public String getTipo() {
+		return "Livro";
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + isbn + ", Ano: " + anoPublicacao;
+	}
+	
+		
 
 }
