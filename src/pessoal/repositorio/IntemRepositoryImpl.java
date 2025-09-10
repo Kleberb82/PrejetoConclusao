2e403be45f65a1597ce30dd19306adff871ad91d
Kleberb82 <kleberbezerra@gmail.com>
2025-09-10 18:11:01 -0300

    Atualizacao ItemRepositoryImpl

--- src/pessoal/repositorio/IntemRepositoryImpl.java
+++ src/pessoal/repositorio/IntemRepositoryImpl.java
@@ -1,5 +1,32 @@
 package pessoal.repositorio;
 
-public class IntemRepositoryImpl {
+import pessoal.modelo.Item;
+import java.util.ArryList;
+import java.util.List;
+import java.util.stream.collectors;
+
+
+public class IntemRepositoryImpl implements ItemReporsitory{
+	private List<Item> itens;
+	
+	public ItemRepositoryImpl() {
+		this.itens = new ArrayLst<>();
+		}
+	
+	@Override
+	public void adicionarItem(Item item) {
+		this.item.add(item);
+	}
+	
+	@Override
+	public List<Item> listarTodosItens(){
+		return new ArrayList<>(this.itens);
+	}
+	
+	@Override
+	public boolean removerItem(String titulo) {
+		return this.itens.removeif(item ->
+		item.getTitulo()egualsIgnoreCase (titulo));
+	}
 
 }
