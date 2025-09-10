aaa17065e602477cc42ffdaf136c7ce50797bc94
Kleberb82 <kleberbezerra@gmail.com>
2025-09-10 17:52:43 -0300

    Atualização de Intem Repository

--- src/pessoal/repositorio/IntemRepository.java
+++ src/pessoal/repositorio/IntemRepository.java
@@ -1,5 +1,16 @@
 package pessoal.repositorio;
 
-public class IntemRepository {
+import pessoal.modelo.Item;
 
+import java.util.List;
+
+public interface IntemRepository {
+
+	void adicionarItem (item item);
+	List<Item> listarTodositens();
+	List<Item> listarItensPorCategoria(String Categoria);
+	boolean removerItem (String titulo);
+	
+	
+	
 }
