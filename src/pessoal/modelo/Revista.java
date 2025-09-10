f4fc3681a46fd99288829e7cf51f03b062948fc1
Kleberb82 <kleberbezerra@gmail.com>
2025-09-10 17:18:24 -0300

    Atualização da classe Revista

--- src/pessoal/modelo/Revista.java
+++ src/pessoal/modelo/Revista.java
@@ -1,5 +1,41 @@
 package pessoal.modelo;
 
-public class Revista {
+public class Revista extends Item {
+	private int numeroEdicao;
+	private String datePublicacao;
+	
+	public Revista (String titulo, String autorArtista, Categoria categoria, int 
+			numeroEdicao, String datePublicacao) {
+		super (titulo, autoArtista, categoria);
+		this.numeroEdicao = numeroEdicao;
+		this.datePublicacao = datePublicacao;
+		}
+	
+	public int getNumeriEdicao() {
+		return numeroEdicao;
+	}
+	
+	public void setNumeroEdicao(int numeroEdicao) {
+		this.numeroEdicao = numeroEdicao;
+	}
+	
+	public String getDatePublicacao() {
+		retun datePublicacao;
+	}
+	
+	public void setDataPublicacao (String datePublicacao) {
+		this.datePublicacao = datePublicacao;
+	}
+	
+	@Override
+	public String getTipo() {
+		return "Revista";
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + ". Tipo: Revista, Edição: " + numeroEdicao + ", Data: " + datePublicacao;
+	}
+	
 
 }
